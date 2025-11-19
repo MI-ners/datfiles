66be55ec3aeca1595b0eb5bd8fa52240b76e4da7
MI-ners <pipip9077@gmail.com>
2025-11-18 17:37:18 -0800

    fixed treehitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
 		config = function()
 			local config = require("nvim-treesitter.configs")
 			config.setup({
+				ensure_installed = { "c", "cpp", "lua", "python", "vim", "bash", "make", "gitcommit" },
 				auto_install = true,
 				highlights = { enable = true },
 				indent = { enable = true },
